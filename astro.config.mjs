56d0a04bdda1371bde184b32d6047025e15827c3
VICTORFBP <victor.fbperez@gmail.com>
2026-06-22 12:04:35 -0500

    feat: add @astrojs/cloudflare adapter to astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
