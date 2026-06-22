03ab09ba8940bd98d08d2733d894c98fe12b269b
VICTORFBP <victor.fbperez@gmail.com>
2026-06-22 12:06:52 -0500

    chore: configure cloudflare adapter in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,14 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://amigurumi-pets.pages.dev',
   output: 'server',
+  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [sitemap()]
 });
